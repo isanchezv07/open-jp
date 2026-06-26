f83eb8b612c28979609bb2d046c3f4d5a5f9948b
isanchezv07 <sanchezisac2007@gmail.com>
2026-06-25 18:24:12 -0600

    Resolve versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,56 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
-import AstroPWA from '@vite-pwa/astro';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    AstroPWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'open-jp',
-        short_name: 'open-jp',
-        description: 'A Japanese practice app',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/favicon.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          },
-          {
-            src: '/favicon.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          }
-        ]
-      },
-      workbox: {
-        navigateFallback: '/404',
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\//]
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true
-      }
-    })
   ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+
+        manifest: {
+          name: 'open-jp',
+          short_name: 'open-jp',
+          description: 'A Japanese practice app',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
+
+          icons: [
+            {
+              src: '/favicon.svg',
+              sizes: '192x192',
+              type: 'image/svg+xml'
+            },
+            {
+              src: '/favicon.svg',
+              sizes: '512x512',
+              type: 'image/svg+xml'
+            }
+          ]
+        },
+
+        workbox: {
+          navigateFallback: '/404',
+          globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
+        },
+
+        devOptions: {
+          enabled: true,
+          navigateFallbackAllowlist: [/^\//]
+        }
+      })
+    ]
   },
+
   devToolbar: {
     enabled: false,
   },
